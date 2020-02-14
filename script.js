dc0369b01fe77fd1381d50641253a253762c80c1
Lopez Javier <20203@columbushs.com>
2020-02-14 13:40:27 -0500

    WIP

---
+++ script.js
@@ -0,0 +1,16 @@
+//Declaring the different colored panels
+
+const topRight = document.getElementById("quarterCircleTopRight");
+const topLeft = document.getElementById("quarterCirlceTopLeft");
+const bottomRight = document.getElementById("quarterCirlceBottomRight");
+const bottomLeft = document.getElementById("quarterCirlceBottomLeft");
+
+//Declaring the variables that will be used through the game
+
+let start
+let order = [];
+let playerOrder = []
+let success;
+let currentTurn;
+let fail;
+
