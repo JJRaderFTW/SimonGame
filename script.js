6c71ddb9389192cb537ae78594f76a93471bd856
Lopez Javier <20203@columbushs.com>
2020-02-18 09:40:19 -0500

    wip

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const topRight = document.getElementById("quarterCircleTopRight");
 const topLeft = document.getElementById("quarterCirlceTopLeft");
 const bottomRight = document.getElementById("quarterCirlceBottomRight");
 const bottomLeft = document.getElementById("quarterCirlceBottomLeft");
+const startButton = document.getElementById("startButton");
 
 //Declaring the variables that will be used through the game
 
@@ -13,4 +14,42 @@ let playerOrder = []
 let success;
 let currentTurn;
 let fail;
+let flash;
+let gameTimer;
+let buttonContent = startButton.innerHTML
 
+
+startButton.addEventListener("click", function(){
+    startGame();
+    if(buttonContent === "Start"){
+        setStop();
+    }
+    else{
+        setStart();
+    }
+});
+
+function setStop(){
+    startButton.innerHTML = "Stop";
+}
+
+function setStart(){
+    start.Button.innerHTML = "Start";
+}
+
+function startGame(){
+    order = [];
+    playerOrder= []
+    flash;
+    currentTurn = 1;
+    successs = false;
+    fail = false;
+
+    //generates order of pattern
+    for(i=0; i<15; i++){
+        order.push(Math.floor(Math.random()*4+1));
+    }
+    
+
+
+}
